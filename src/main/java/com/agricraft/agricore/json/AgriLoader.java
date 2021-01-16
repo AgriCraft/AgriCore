34efab7371cc18841763992af58e19326dd45bc6
InfinityRaider <theoneandonlyraider@gmail.com>
2021-01-16 10:52:35 +0000

    Split reading and parsing of the json to avoid reading from file twice

--- src/main/java/com/agricraft/agricore/json/AgriLoader.java
+++ src/main/java/com/agricraft/agricore/json/AgriLoader.java
@@ -38,7 +38,11 @@ public final class AgriLoader {
             return;
         }
         // Attempt to load element. If fails, return.
-        T obj = parseVersionAware(location, registry.getElementVersion());
+        JsonElement json = readJson(location);
+        if (json == null) {
+            return;
+        }
+        T obj = parseVersionAware(json, location, registry.getElementVersion());
         if(obj != null) {
             obj.setPath(root.relativize(location).toString().replaceAll("\\\\", "/"));
         } else {
@@ -53,21 +57,26 @@ public final class AgriLoader {
         registry.registerElement(obj);
     }
 
-    @Nullable
-    private static <T extends AgriSerializable & Comparable<T>> T parseVersionAware(Path location, AgriJsonVersion<T> version) {
-        // Parse the version
-        String jsonVersion;
-        try {
-            jsonVersion = new JsonParser().parse(Files.newBufferedReader(location)).getAsJsonObject().get("version").getAsString();
-        } catch(Exception e) {
-            // If no version is defined in the json, it is the first version: 1.12
-            jsonVersion = Versions_1_12.VERSION;
-            AgriCore.getCoreLogger().warn("Could not identify version for {0}, assuming default version ({1}).",
-                    location.getFileName(), jsonVersion);
+    private static JsonElement readJson(Path location) {
+        try (Reader reader = Files.newBufferedReader(location)) {
+            return new JsonParser().parse(reader);
+        } catch (IOException | JsonParseException | NullPointerException e) {
+            // IOException is thrown when unable to read file.
+            // JsonParseExeption is thrown when JSON format is wrong.
+            // NullPointerException is thrown when the file is empty.
+            AgriCore.getCoreLogger().warn("Unable to read json: \"{0}\"!", location);
+            AgriCore.getCoreLogger().trace(e);
+            return null;
         }
+    }
+
+    @Nullable
+    private static <T extends AgriSerializable & Comparable<T>> T parseVersionAware(JsonElement json, Path location, AgriJsonVersion<T> version) {
+        // Parse the version
+        String jsonVersion = parseVersion(json, location);
         if(version.descriptor().equals(jsonVersion)) {
             // Same version, can directly parse
-            return parse(location, version.getElementClass());
+            return parse(json, location, version.getElementClass());
         } else {
             // Wrong version, recursively convert to required version
             AgriCore.getCoreLogger().warn("Attempting to parse {0} from an older version ({1}), required version is {2}.",
@@ -77,7 +86,7 @@ public final class AgriLoader {
             // Check if valid version conversion exists
             if(prevVersion != null && converter != null) {
                 // Recursively parse from the previous version
-                AgriSerializable prev = parseVersionAware(location, prevVersion);
+                AgriSerializable prev = parseVersionAware(json, location, prevVersion);
                 if(prev != null) {
                     // Back up before conversion
                     Path backupPath = Paths.get(location.toString().replaceAll("defaults", "backups/" + prevVersion.descriptor()));
@@ -100,19 +109,29 @@ public final class AgriLoader {
                 // No version conversion defined, this means that the new version should be compatible with the old version
                 AgriCore.getCoreLogger().warn("No conversion from version {0} to version {1} exists for this file, parsing with latest version.",
                         location.getFileName(), jsonVersion, version.descriptor());
-                return parse(location, version.getElementClass());
+                return parse(json, location, version.getElementClass());
             }
         }
     }
 
-    private static <T extends AgriSerializable & Comparable<T>> T parse(Path location, Class<T> clazz) {
-        try (Reader reader = Files.newBufferedReader(location)) {
-            return GSON.fromJson(reader, clazz);
-        } catch (IOException | JsonParseException | NullPointerException e) {
-            // IOException is thrown when unable to read file.
+    private static String parseVersion(JsonElement json, Path location) {
+        JsonObject jsonObject = json.getAsJsonObject();
+        if(jsonObject.has("version")) {
+            return jsonObject.get("version").getAsString();
+        } else {
+            // If no version is defined in the json, it is the first version: 1.12
+            AgriCore.getCoreLogger().warn("Could not identify version for {0}, assuming default version ({1}).",
+                    location.getFileName(), Versions_1_12.VERSION);
+            return Versions_1_12.VERSION;
+        }
+    }
+
+    private static <T extends AgriSerializable & Comparable<T>> T parse(JsonElement json, Path location, Class<T> clazz) {
+        try {
+            return GSON.fromJson(json, clazz);
+        } catch (JsonParseException | NullPointerException e) {
             // JsonParseExeption is thrown when JSON format is wrong.
-            // NullPointerException is thrown when the file is empty.
-            AgriCore.getCoreLogger().warn("Unable to load Element: \"{0}\"!", location);
+            AgriCore.getCoreLogger().warn("Unable to parse json: \"{0}\"!", location);
             AgriCore.getCoreLogger().trace(e);
             return null;
         }
