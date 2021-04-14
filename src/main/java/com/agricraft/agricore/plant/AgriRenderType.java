7635a30b243b44d4c0d6d9235c7bac0f5527fadd
InfinityRaider <theoneandonlyraider@gmail.com>
2021-04-14 14:57:08 +0100

    Add height modifier to plant render types

--- src/main/java/com/agricraft/agricore/plant/AgriRenderType.java
+++ src/main/java/com/agricraft/agricore/plant/AgriRenderType.java
@@ -9,18 +9,28 @@ public enum AgriRenderType {
     @SerializedName(value = "cross", alternate = {"CROSS", "x", "X"})
     CROSS('X'),
     @SerializedName(value = "plus", alternate = {"PLUS", "+"})
-    PLUS('+'),
+    PLUS('+', 0.75F),
     @SerializedName(value = "rhombus", alternate = {"RHOMBUS", "⬦", "◇"})
     RHOMBUS('◇');
 
     private final char id;
+    private final float heightModifier;
 
     AgriRenderType(char id) {
+        this(id, 1.0F);
+    }
+
+    AgriRenderType(char id, float heightModifier) {
         this.id = id;
+        this.heightModifier = heightModifier;
     }
 
     public char id() {
         return id;
+    }
+
+    public float getHeightModifier() {
+        return this.heightModifier;
     }
 
     @Override
