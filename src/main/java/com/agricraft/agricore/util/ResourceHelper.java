3dd97bdb019e6c4eec58bf34120e519fb1441757
InfinityRaider <theoneandonlyraider@gmail.com>
2021-04-20 09:20:10 +0100

    Optimize ResourceHelper memory usage
    Fixes #10

--- src/main/java/com/agricraft/agricore/util/ResourceHelper.java
+++ src/main/java/com/agricraft/agricore/util/ResourceHelper.java
@@ -6,16 +6,48 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
 
 public class ResourceHelper {
+    /**
+     * Copies all resources in the class path matching the given name predicate, as well as the file structure predicate to a new directory
+     *
+     * @param nameFilter the predicate for the file name
+     * @param dirFilter the predicate for the file (directory) structure
+     * @param toFunction function specifying the destination where each file should be copied to
+     * @param overwrite specifies if files which already exist should be overwritten or not
+     */
+    public static void copyResources(Predicate<String> nameFilter, Predicate<String> dirFilter, Function<String, Path> toFunction, boolean overwrite) {
+        ResourceHelper helper = new ResourceHelper();
+        helper.findResources(nameFilter).stream()
+                .filter(dirFilter)
+                .forEach(r -> helper.copyResource(r, toFunction.apply(r), overwrite)
+                );
+    }
 
-    private static final Reflections REFLECTIONS = new Reflections(null, new ResourcesScanner());
+    /**
+     * Reflections instance used to find jsons
+     */
+    private final Reflections reflections;
 
-    public static Set<String> findResources(Predicate<String> nameFilter) {
-        return REFLECTIONS.getResources(nameFilter::test);
+    /**
+     * The Reflections object can use quite some memory, therefore we instantiate a new one when we need it,
+     * and discard it afterwards
+     */
+    protected ResourceHelper() {
+        this.reflections = new Reflections(null, new ResourcesScanner());
+    }
+
+    /**
+     * Finds all resources in the class path matching the given predicate
+     * @param nameFilter file name predicate
+     * @return set of all filenames matching the name filter
+     */
+    protected Set<String> findResources(Predicate<String> nameFilter) {
+        return this.reflections.getResources(nameFilter::test);
     }
 
     /**
@@ -27,11 +59,11 @@ public class ResourceHelper {
      * @param to the location to copy the resource to.
      * @param overwrite if the copy task should overwrite existing files.
      */
-    public static void copyResource(String from, Path to, boolean overwrite) {
+    protected void copyResource(String from, Path to, boolean overwrite) {
         try {
             if (overwrite || !Files.exists(to)) {
                 Files.createDirectories(to.getParent());
-                Files.copy(getResourceAsStream(from), to, StandardCopyOption.REPLACE_EXISTING);
+                Files.copy(this.getResourceAsStream(from), to, StandardCopyOption.REPLACE_EXISTING);
             }
         } catch (Exception e) {
             AgriCore.getLogger("AgriCraft").error(
@@ -50,7 +82,7 @@ public class ResourceHelper {
      * @param location the location of the desired resource stream.
      * @return the resource, as a stream.
      */
-    public static InputStream getResourceAsStream(String location) {
+    protected InputStream getResourceAsStream(String location) {
         InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(location);
         return in != null ? in : ResourceHelper.class.getResourceAsStream(location);
     }
